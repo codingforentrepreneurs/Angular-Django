8a28bb7ec1ef62a7f34e2d5bb4dc5c2ec7eac22a
CFE <hello@teamcfe.com>
2017-04-24 10:14:31 -0700

    9 - Auto Generate Slugs

--- backend/src/videos/models.py
+++ backend/src/videos/models.py
@@ -1,23 +1,26 @@
 from django.db import models
+from django.db.models.signals import pre_save
 
-# Create your models here.
+from .utils import unique_slug_generator
 
 class Video(models.Model):
     name        = models.CharField(max_length=220)
     slug        = models.SlugField(unique=True, blank=True)
-    embed       = models.CharField(max_length=120, null=True, blank=True)
+    embed       = models.CharField(max_length=120, help_text='YouTube embed code', null=True, blank=True)
     featured    = models.BooleanField(default=False)
     timestamp   = models.DateTimeField(auto_now_add=True)
 
     def __str__(self): # __unicode__
         return self.name
 
-'''
-{
- "name": "Welcome",
- "slug": "item-1",
- "embed": "1hyjLD7pk10",
- "image": "/static/ang/assets/images/nature/4.jpg",
- "featured": true
-}
-'''
+    @property
+    def title(self):
+        return self.name
+
+
+def video_pre_save_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+
+pre_save.connect(video_pre_save_receiver, sender=Video)
