83372d9242d1456cdab9c56ac468553bda57413e
CFE <hello@teamcfe.com>
2017-04-26 17:17:16 -0700

    24 - Env Config Variables

--- backend/src/tryangular/aws/conf.py
+++ backend/src/tryangular/aws/conf.py
@@ -1,25 +1,27 @@
 import datetime
-AWS_ACCESS_KEY_ID = "AKIAILCHSZUQTXWBFHNA"
-AWS_SECRET_ACCESS_KEY = "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"
-AWS_FILE_EXPIRE = 200
-AWS_PRELOAD_METADATA = True
-AWS_QUERYSTRING_AUTH = True
+import os
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')  #"AKIAILCHSZUQTXWBFHNA"
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY') #"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"
+if AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY:
+    AWS_FILE_EXPIRE = 200
+    AWS_PRELOAD_METADATA = True
+    AWS_QUERYSTRING_AUTH = True
 
-DEFAULT_FILE_STORAGE = 'tryangular.aws.utils.MediaRootS3BotoStorage'
-STATICFILES_STORAGE = 'tryangular.aws.utils.StaticRootS3BotoStorage'
-AWS_STORAGE_BUCKET_NAME = 'angulardjango'
-S3DIRECT_REGION = 'us-west-2'
-S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
-MEDIA_ROOT = MEDIA_URL
-STATIC_URL = S3_URL + 'static/'
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+    DEFAULT_FILE_STORAGE = 'tryangular.aws.utils.MediaRootS3BotoStorage'
+    STATICFILES_STORAGE = 'tryangular.aws.utils.StaticRootS3BotoStorage'
+    AWS_STORAGE_BUCKET_NAME = 'angulardjango'
+    S3DIRECT_REGION = 'us-west-2'
+    S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+    MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+    MEDIA_ROOT = MEDIA_URL
+    STATIC_URL = S3_URL + 'static/'
+    ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
-two_months = datetime.timedelta(days=61)
-date_two_months_later = datetime.date.today() + two_months
-expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
+    two_months = datetime.timedelta(days=61)
+    date_two_months_later = datetime.date.today() + two_months
+    expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
 
-AWS_HEADERS = { 
- 'Expires': expires,
- 'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
-}
+    AWS_HEADERS = { 
+     'Expires': expires,
+     'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
+    }
